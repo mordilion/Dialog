83b3b69d541edebd389e6f80a8be39e628489193
Henning Huncke <mordilion@gmx.de>
2017-12-11 16:21:34 -0600

    finalized the HtmlFormatter

--- src/Dialog/Formatter/HtmlFormatter.php
+++ src/Dialog/Formatter/HtmlFormatter.php
@@ -58,8 +58,8 @@ class HtmlFormatter extends FormatterAbstract
 
         $html .= $this->getRow('Message', $data['message']);
         $html .= $this->getRow('Time', $data['datetime']);
-        $html .= $this->getRow('Context', $data['context']);
-        $html .= $this->getRow('Additional', $data['additional']);
+        $html .= $this->getRow('Context', $this->normalizeArray($record->getContext(), true));
+        $html .= $this->getRow('Additional', $this->normalizeArray($record->getAdditional(), true));
 
         $html .= '  </table>';
         $html .= '</div>';
@@ -67,38 +67,33 @@ class HtmlFormatter extends FormatterAbstract
         return $html;
     }
 
-    protected function getCss()
+    /**
+     * Returns the default CSS for the HTML layout.
+     *
+     * @return string
+     */
+    public function getCss()
     {
         $color = isset($this->levelColors[$level]) ? $this->levelColors[$level] : '#cccccc';
-        $css   = '
-            .dialog {
-                background-color: #ffffff;
-                color: #000000;
-            }
-        ';
+        $css   = '.dialog { background-color: #ffffff; color: #000000; margin-bottom: 40px; } .dialog table th, .dialog table th { padding: 5px 8px; text-align: left; }';
 
         foreach ($this->levelColors as $level => $color) {
-            $css .= '
-                .dialog .dialog-title-' . $level . ' {
-                    background-color: ' . $color . ';
-                    color: #ffffff;
-                }
-            ';
+            $css .= '.dialog .dialog-title-' . $level . ' { background-color: ' . $color . '; color: #ffffff; padding: 5px 10px; }';
         }
 
-        $css .= '
-            .dialog .dialog-row-header {
-                background-color: #ecf0f1;
-            }
-
-            .dialog .dialog-row-description {
-                background-color: #ffffff;
-            }
-        ';
+        $css .= '.dialog .dialog-row-header { background-color: #ecf0f1; } .dialog .dialog-row-description { background-color: #ffffff; }';
 
         return $css;
     }
 
+    /**
+     * Returns the HTML code for a h1 title tag.
+     *
+     * @param string $level
+     * @param string $title
+     *
+     * @return string
+     */
     protected function getTitle($level, $title)
     {
         $title = htmlspecialchars($title, ENT_NOQUOTES, 'UTF-8');
@@ -106,7 +101,16 @@ class HtmlFormatter extends FormatterAbstract
         return '<h1 class="dialog-title-' . $level . '">' . $title . '</h1>';
     }
 
-    protected function getRow($header, $data, $escapeDescription = true)
+    /**
+     * Returns the HTML code for one table row.
+     *
+     * @param string $header
+     * @param string $description
+     * @param boolean $escapeDescription
+     *
+     * @return string
+     */
+    protected function getRow($header, $description, $escapeDescription = true)
     {
         $header = htmlspecialchars($header);
 
