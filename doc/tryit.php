3059d9d895a1f5552de68c27117c0c52c2835498
Henning Huncke <mordilion@gmx.de>
2018-04-18 12:36:05 +0200

    added a using of a condition

--- doc/tryit.php
+++ doc/tryit.php
@@ -2,10 +2,18 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+
 use Dialog\Logger;
 use Dialog\Handler\StreamHandler;
+use Dialog\Condition\DatetimeCondition;
+
 
 $logger = new Logger();
 $logger->addHandler((new StreamHandler())->setUrl(__DIR__ . '/tryit.log'));
+$logger->addHandler(
+    (new StreamHandler())->setUrl(__DIR__ . '/tryit-with-conditions.log')
+        ->addCondition((new DatetimeCondition())->setTimezone('America/Chicago')->setValue('01:00AM')->setOperator('>='))
+);
+
 
 $logger->info('Feel free to use this file to try Dialog and play a bit with it! :)');
