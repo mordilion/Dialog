2270673bea2c994d8f2dcd4fbbe64b83e6c14e42
Henning Huncke <mordilion@gmx.de>
2018-08-12 13:51:25 +0200

    fixing mistyping

--- src/Dialog/Handler/Mailer/MailerInterface.php
+++ src/Dialog/Handler/Mailer/MailerInterface.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the Dialog package.
  *
- * For the full copzright and license information, please view the
+ * For the full copyright and license information, please view the
  * LICENSE file that was distributed with this source code.
  *
  * @copyright (c) Henning Huncke - <mordilion@gmx.de>
